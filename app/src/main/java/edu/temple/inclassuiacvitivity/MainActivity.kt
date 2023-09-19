67e396795a671be7511febe2dbe9dfacddd2dc97
heronziegel <tuo12440@temple.edu>
2023-09-19 14:57:11 -0400

    Fixed an error in the numberArray to display the numbers 1-100 correctly

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,14 +14,18 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        val numberArray = Array(100){i -> (i*10)}
+        val numberArray = Array(100){i -> (i) + 1}
 
         /* Step 2: Create adapter to display items from array in Spinner */
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object:
+//        spinner.onItemSelectedListener = object: OnItemSelectedListener {
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long){
+//
+//            }
+//        }
 
     }
 }
