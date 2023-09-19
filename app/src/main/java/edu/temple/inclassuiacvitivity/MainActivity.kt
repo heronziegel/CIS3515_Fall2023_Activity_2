33eda432b836b344866d12917f30845ecdd973ef
heronziegel <tuo12440@temple.edu>
2023-09-19 15:10:36 -0400

    Overrode the onItemSelectedListener for the spinner so that it updates the text size based on selection from the array

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +22,18 @@ class MainActivity : AppCompatActivity() {
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-//        spinner.onItemSelectedListener = object: OnItemSelectedListener {
-//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long){
-//
-//            }
-//        }
+        spinner.onItemSelectedListener = object: OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long){
+                p1?.run {
+                    val num = (this as TextView).text.toString().toFloat()
+                    displayTextView.textSize = num
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
 
     }
 }
